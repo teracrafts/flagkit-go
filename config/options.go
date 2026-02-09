1390e7cddfe29db0c87fbc0f3d60a8b9ce78b925
Kingsley Eze <firebox@ikingsley.com>
2026-02-10 00:31:54 +0100

    chore: bump version to 1.0.8

--- config/options.go
+++ config/options.go
@@ -55,7 +55,7 @@ const (
 	DefaultRetries = 3
 
 	// SDKVersion is the current SDK version.
-	SDKVersion = "1.0.7"
+	SDKVersion = "1.0.8"
 )
 
 // Options configures the FlagKit client.
--- internal/http/client.go
+++ internal/http/client.go
@@ -44,7 +44,7 @@ var NewErrorWithCause = types.NewErrorWithCause
 var NetworkError = types.NetworkError
 
 // SDKVersion should be set by the main package.
-var SDKVersion = "1.0.7"
+var SDKVersion = "1.0.8"
 
 // defaultBaseURL is the internal base URL for the FlagKit API.
 const defaultBaseURL = "https://api.flagkit.dev/api/v1"
