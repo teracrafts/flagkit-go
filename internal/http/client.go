24b2031d5d05c5e23319a45f9a56edad88716657
Kingsley Eze <firebox@ikingsley.com>
2026-02-10 00:13:51 +0100

    chore: bump version to 1.0.7

--- internal/http/client.go
+++ internal/http/client.go
@@ -44,7 +44,7 @@ var NewErrorWithCause = types.NewErrorWithCause
 var NetworkError = types.NetworkError
 
 // SDKVersion should be set by the main package.
-var SDKVersion = "1.0.6"
+var SDKVersion = "1.0.7"
 
 // defaultBaseURL is the internal base URL for the FlagKit API.
 const defaultBaseURL = "https://api.flagkit.dev/api/v1"
